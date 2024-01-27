858c6e2ce9427f5d2129641703129ffa066c031f
Finnerex <61033009+Finnerex@users.noreply.github.com>
2024-01-27 15:21:57 -0500

    line

--- src/objects.c
+++ src/objects.c
@@ -6,7 +6,7 @@ vec2_t square_norms[8];
 obj_t square_data = {square_norms, square_faces, square_verts, 3, 4};
 
 face_t face_face[1] = {{0, 1}};
-vec2_t face_verts[2] = {{CENTER_X + 20, CENTER_Y + 20}, {CENTER_X - 20, CENTER_Y - 20}};
+vec2_t face_verts[2] = {{CENTER_X + 30, CENTER_Y + 30}, {CENTER_X - 10, CENTER_Y - 10}};
 vec2_t face_norms[1];
 obj_t face_data = {face_norms, face_face, face_verts, 1, 2};
 
